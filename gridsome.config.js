6066912538d4b163a62ee656cfa83d972dc56ab4
Paul Mtali <paul.mtali@techie.com>
2020-03-12 15:06:11 +0200

    update site settings

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,11 +2,11 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
+  siteName: "Chrina's",
+  siteUrl: `https://draft.chrinas.com`,
   host: "0.0.0.0",
-  titleTemplate: "%s - NWA",
-  siteDescription: "Creative technologist",
+  titleTemplate: "%s - Chrina's Guest House",
+  siteDescription: "Feel at Home Away from Home",
   plugins: [
     {
       use: "@gridsome/source-filesystem",
